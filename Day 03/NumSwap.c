8019b2b5556a14f3833cd899984601fd40898bcc
Rudra N. Gholba <76525129+RudraGholba@users.noreply.github.com>
2025-09-21 18:22:19 +0530

    Update NumSwap.c

--- Day 03/NumSwap.c
+++ Day 03/NumSwap.c
@@ -1,18 +1,11 @@
-// The following code is incomplete.
 #include <stdio.h>
-#include <string.h>
 int main()
 {
-    char num[];
-    printf("Enter two numbers with a space\n");
-    scanf("%s",&num);
-    char *s;
-    s=strtok(num," ");
-    while (s!= NULL){
-       printf("%s",s);
-       s=strtok(NULL,"-");
-    }
-
+    int c;
+    printf("Enter temprature in Celsius\n");
+    scanf("%d",&c);
+    int f;
+    f=(c*(1.8)+32);
+    printf("Fahrenheit=%d\n",f);
     return 0;
 }
-
