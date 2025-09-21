b11713cabe584c8d6c9866ee70e91381feb23a5c
Rudra N. Gholba <76525129+RudraGholba@users.noreply.github.com>
2025-09-21 18:24:10 +0530

    Update Sl-Cl.c

--- Day 05/Sl-Cl.c
+++ Day 05/Sl-Cl.c
@@ -1,4 +1,3 @@
-//This code is incomplete
 #include <stdio.h>
 #include <math.h>
 int main()
@@ -6,20 +5,20 @@ int main()
     int p;
     printf("Enter Principle value\n");
     scanf("%d",&p);
-    int r;
+    float r;
     printf("Enter Rate\n");
-    scanf("%d",&r);
+    scanf("%f",&r);
     int t;
     printf("Enter Time\n");
     scanf("%d",&t);
     int si;
-    si=p*r*t;
-    int r1;
-    r1=(r+1);
-    int ex;
+    si=(p*r*t)/100;
+    float r1;
+    r1=((r/100.0)+1);
+    float ex;
     ex=pow(r1, t);
     float ci;
     ci=((p*ex)-p);
-    printf("Simple intrest is %d\nCompound intrest id%f ",si,ci);
+    printf("Simple intrest is %d\nCompound intrest is %.2f ",si,ci);
     return 0;
 }
